6ad07f0f212b2a8ca531375948f790e6a411aefd
zyh-tech <1463917744@qq.com>
2023-07-22 17:14:29 +0800

    CPU原子操作原理

--- 并发操作的同步/线程闩latch.cpp
+++ 并发操作的同步/线程闩latch.cpp
@@ -4,16 +4,16 @@
 
 
 void foo(){
-  unsigned const thread_count=...;
-  latch done(thread_count); // 1
-  my_data data[thread_count];
-  std::vector<std::future<void> > threads;
-  for(unsigned i=0;i<thread_count;++i)
-    threads.push_back(std::async(std::launch::async,[&,i]{ // 2
-      data[i]=make_data(i);
-      done.count_down(); // 3
-      do_more_stuff(); // 4
-    }));
-  done.wait(); // 5
-  process_data(data,thread_count); // 6
+    unsigned const thread_count=...;
+    latch done(thread_count); // 1
+    my_data data[thread_count];
+    std::vector<std::future<void> > threads;
+    for(unsigned i=0;i<thread_count;++i)
+        threads.push_back(std::async(std::launch::async,[&,i]{ // 2
+            data[i]=make_data(i);
+            done.count_down(); // 3
+            do_more_stuff(); // 4
+        }));
+    done.wait(); // 5
+    process_data(data,thread_count); // 6
 } // 7
